a4257c53733dbf4b035999a6987b7c73880f381b
wwwil <wwwil.squires@gmail.com>
2022-06-13 21:04:05 +0100

    Update and set tag

--- build-multiarch.sh
+++ build-multiarch.sh
@@ -4,27 +4,28 @@ set -o errexit
 set -o pipefail
 
 DOCKER_IMAGE="wwwil/pms-docker"
+DOCKER_TAG="1.22.1.4228-724c56e62"
 
-docker build --tag "${DOCKER_IMAGE}:amd64" \
-  --build-arg TAG=latest \
+docker build --tag "${DOCKER_IMAGE}:${DOCKER_TAG}-amd64" \
+  --build-arg TAG="${DOCKER_TAG}" \
   --file Dockerfile .
 
-docker build --tag "${DOCKER_IMAGE}:arm64v8" \
-  --build-arg TAG=latest \
+docker build --tag "${DOCKER_IMAGE}:${DOCKER_TAG}-arm64v8" \
+  --build-arg TAG="${DOCKER_TAG}" \
   --file Dockerfile.arm64 .
 
-docker build --tag "${DOCKER_IMAGE}:arm32v7" \
-  --build-arg TAG=latest \
+docker build --tag "${DOCKER_IMAGE}:${DOCKER_TAG}-arm32v7" \
+  --build-arg TAG="${DOCKER_TAG}" \
   --file Dockerfile.armv7 .
 
 if [[ $1 == "--push" ]]; then
-  docker push "${DOCKER_IMAGE}:amd64"
-  docker push "${DOCKER_IMAGE}:arm64v8"
-  docker push "${DOCKER_IMAGE}:arm32v7"
+  docker push "${DOCKER_IMAGE}:${DOCKER_TAG}-amd64"
+  docker push "${DOCKER_IMAGE}:${DOCKER_TAG}-arm64v8"
+  docker push "${DOCKER_IMAGE}:${DOCKER_TAG}-arm32v7"
   docker manifest create \
-    "${DOCKER_IMAGE}:latest" \
-    --amend "${DOCKER_IMAGE}:amd64" \
-    --amend "${DOCKER_IMAGE}:arm64v8" \
-    --amend "${DOCKER_IMAGE}:arm32v7"
-  docker manifest push "${DOCKER_IMAGE}:latest"
+    "${DOCKER_IMAGE}:${DOCKER_TAG}" \
+    --amend "${DOCKER_IMAGE}:${DOCKER_TAG}-amd64" \
+    --amend "${DOCKER_IMAGE}:${DOCKER_TAG}-arm64v8" \
+    --amend "${DOCKER_IMAGE}:${DOCKER_TAG}-arm32v7"
+  docker manifest push "${DOCKER_IMAGE}:${DOCKER_TAG}"
 fi
